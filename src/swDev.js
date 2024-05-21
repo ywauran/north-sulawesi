b07667d18f0955e52fae99b9038ab420029754af
Yohanes Harke Wauran <harkewauran23@gmail.com>
2024-05-21 09:21:36 +0800

    add data

--- src/swDev.js
+++ src/swDev.js
@@ -1,5 +1,3 @@
-/* eslint-disable prettier/prettier */
-/* eslint-disable no-undef */
 export default function swDev() {
   let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
   navigator.serviceWorker.register(swUrl).then((Response) => {
